5ea3cd4b5786df81d2ad7d88e3ed7c909362d9da
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2026-09-07 15:41:03 +0600

    ✨ add packages/react/src/index.ts

---
+++ packages/react/src/index.ts
@@ -0,0 +1,20 @@
+export { Carousel } from "./Carousel.tsx";
+export type { CarouselControlsOptions, CarouselProps } from "./Carousel.tsx";
+export { CarouselViewport } from "./CarouselViewport.tsx";
+export type { CarouselViewportProps } from "./CarouselViewport.tsx";
+export { CarouselTrack } from "./CarouselTrack.tsx";
+export type { CarouselTrackProps } from "./CarouselTrack.tsx";
+export { CarouselSlide } from "./CarouselSlide.tsx";
+export type { CarouselSlideProps } from "./CarouselSlide.tsx";
+export { CarouselPrevious, CarouselNext } from "./CarouselNav.tsx";
+export type { CarouselNavButtonProps } from "./CarouselNav.tsx";
+export { CarouselPagination } from "./CarouselPagination.tsx";
+export type { CarouselPaginationProps } from "./CarouselPagination.tsx";
+export { useCarousel, useCarouselOptional } from "./useCarousel.ts";
+export { useCarouselState } from "./useCarouselState.ts";
+export { useCarouselSetup } from "./useCarouselSetup.ts";
+export {
+  CarouselRuntimeContext,
+  CarouselSetupContext,
+} from "./context.ts";
+export type { CarouselSetupContextValue } from "./context.ts";
