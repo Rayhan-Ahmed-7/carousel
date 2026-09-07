a0935aa2cd76cda739e23e1809ec097ec6e513ed
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2026-09-07 15:40:39 +0600

    ✨ add packages/core/src/commands/goTo.ts

---
+++ packages/core/src/commands/goTo.ts
@@ -0,0 +1,8 @@
+import type { Command, CommandContext } from "./Command.ts";
+
+export class GoToCommand implements Command {
+  constructor(private index: number, private animate: boolean = true) {}
+  execute(ctx: CommandContext): void {
+    ctx.goTo(this.index, this.animate);
+  }
+}
